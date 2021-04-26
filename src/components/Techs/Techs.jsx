e390e93b39d69aecd03f07ffcaa607b594f7ad23
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-27 00:29:21 +0600

    feat: add techs__items

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -4,13 +4,17 @@ const Techs = (props) => {
     return (
         <section className="techs page__content page__content_grey">
             <div className="page__section">
-                <h2 className="techs__title title">
-                    Технологии
-                </h2>
-                <div className="techs__info">
-                    <h2 className="techs__subtitle">7 технологии</h2>
-                    <p className="techs__descritpion">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
-                    <div className="techs__items"></div>
+                <h2 className="techs__title title">Технологии</h2>
+                <h3 className="techs__subtitle">7 технологии</h3>
+                <p className="techs__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+                <div className="techs__items items">
+                    <div className="items__item">HTML</div>
+                    <div className="items__item">CSS</div>
+                    <div className="items__item">JS</div>
+                    <div className="items__item">React</div>
+                    <div className="items__item">Git</div>
+                    <div className="items__item">Express.js</div>
+                    <div className="items__item">mongoDB</div>
                 </div>
             </div>
         </section>
