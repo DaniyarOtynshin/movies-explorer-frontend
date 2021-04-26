1e0b5ebc9b6a5755810896368c787ebfa7cc0641
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-26 17:17:19 +0600

    feat: add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+const Header = (props) => {
+    return (
+        <header className="header page__header">
+            <img src="" alt="logo" className="header__log0 logo"/>
+            <div className="header__menu menu"></div>
+        </header>
+    )
+};
+
+export default Header;
