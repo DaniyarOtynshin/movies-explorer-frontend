9f698aeb86b272c7f4d1340e7a0ea7753ebedd89
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-26 17:30:38 +0600

    feat: configure index.css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+
+import Header from '../Header/Header';
+
 function App() {
   return (
     <div className="page__content">
-      
+      <Header />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,15 @@
+import React from 'react';
+
+import logoPath from '../../images/logo.svg';
+
 const Header = (props) => {
     return (
         <header className="header page__header">
-            <img src="" alt="logo" className="header__log0 logo"/>
-            <div className="header__menu menu"></div>
+            <img src={ logoPath } alt="logo" className="header__logo logo"/>
+            <div className="header__menu menu">
+                <button className="menu__button">Войти</button>
+                <a href="#" className="menu__link">Регистрация</a>
+            </div>
         </header>
     )
 };
