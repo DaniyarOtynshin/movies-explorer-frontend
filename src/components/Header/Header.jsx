82ad3eb25f911512ea4c2b01691869867448fb41
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-04-27 23:33:17 +0600

    feat: add Header movies view

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 
 import logoPath from '../../images/logo.svg';
 
+const isMain = false;
+
 const Header = (props) => {
     return (
-        <header className="page__content page__content_green">
+        <header className={ `page__content ${isMain ? "page__content_green" : "page__content_black"}` }>
             <div className="header page__section">
                 <img src={ logoPath } alt="logo" className="header__logo logo"/>
                 <div className="header__menu menu">
-                    <a href="#" className="menu__link">Регистрация</a>
-                    <a href="#" className="menu__link"><button className="menu__button">Войти</button></a>
+                    {
+                        isMain
+                        ? <>
+                            <a href="#" className="menu__link">Регистрация</a>
+                            <a href="#" className="menu__link"><button className="menu__button">Войти</button></a>
+                        </>
+                        : <>
+                            <a href="#" className="menu__link menu__link_movies">Фильмы</a>
+                            <a href="#" className="menu__link menu__link_saved-films">Сохранённые фильмы</a>
+                            <div className="menu__profile-account">
+                                <a href="#" className="menu__link menu__profile-text">Аккаунт</a>
+                                <a href="#" className="menu__link menu__link_profile"><span className="menu__profile"/></a>
+                            </div>
+                        </>
+                    }
+
                 </div>
             </div>
         </header>
