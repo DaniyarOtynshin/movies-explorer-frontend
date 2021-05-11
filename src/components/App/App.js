832b343f1399a7d2f2ac832f9c192175198addd0
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2021-05-11 22:32:54 +0600

    feat: add ProtectedRoute and CurrentUserContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 
+import CurrentUserContext from '../../contexts/CurrentUserContext';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
@@ -11,35 +12,41 @@ import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 
 function App() {
-  return (
-    <div className="page__content">
-      <Route exact path="/">
-        <Header isMain={true}/>
-        <Main />
-      </Route>
-      <Route path="/movies">
-        <Header isMain={false}/>
-        <Movies isSaved={false}/>
-      </Route>
-      <Route path="/saved-movies">
-        <Header isMain={false}/>
-        <Movies isSaved={true}/>
-      </Route>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-      <Route path="/signup">
-        <Register />
-      </Route>
-      <Route path="/signin">
-        <Login />
-      </Route>
-      <Route path="/not-found-error">
-        <NotFoundError />
-      </Route>
-      <Footer />
-    </div>
-  );
+
+    const [currentUser, setCurrentUser] = useState({});
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    return (
+        <CurrentUserContext.Provider value={currentUser}>
+            <div className="page__content">
+                <Route exact path="/">
+                    <Header isMain={true}/>
+                    <Main />
+                </Route>
+                <Route path="/movies">
+                    <Header isMain={false}/>
+                    <Movies isSaved={false}/>
+                </Route>
+                <Route path="/saved-movies">
+                    <Header isMain={false}/>
+                    <Movies isSaved={true}/>
+                </Route>
+                <Route path="/profile">
+                    <Profile />
+                </Route>
+                <Route path="/signup">
+                    <Register />
+                </Route>
+                <Route path="/signin">
+                    <Login />
+                </Route>
+                <Route path="/not-found-error">
+                    <NotFoundError />
+                </Route>
+                <Footer />
+            </div>
+        </CurrentUserContext.Provider>
+    );
 }
 
 export default App;
